c5bcd37eddc7464ed660ecaa281be3837a794c23
Azin <62732881+Azin-kun@users.noreply.github.com>
2020-07-22 21:54:17 +0700

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,11 @@
+var nama = "kkkkkk";
+var harga = 1500000;
+var ukuran1 = 20.5; 
+var ukuran2 = 30;
+var ppn = 15/100;
+
+var HargaTanpappn = ukuran1 * ukuran2 * harga; 
+var hargappn = HargaTanpappn * ppn;
+var hasil = HargaTanpappn + hargappn;
+
+console.log("harga akhirnya yaitu " + hasil);
